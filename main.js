6eab75f32cdae258d98d52e9fd3a4db952a16324
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-07-21 07:25:03 +0530

    EXTREMEFINALPROJECT

--- main.js
+++ main.js
@@ -1,77 +1,70 @@
 var form = document.getElementById('addForm');
-var itemList=document.getElementById('items');
-var filter=document.getElementById('filter')
+    var itemList = document.getElementById('items');
+    var filter = document.getElementById('filter')
 
-// FORM SUBMIT EVENT
+    // FORM SUBMIT EVENT
+    form.addEventListener('submit', addItem);
 
-form.addEventListener('submit', addItem);
+    // DELETE EVENT
+    itemList.addEventListener('click', removeItem);
 
-// DELETE EVENT
-itemList.addEventListener('click', removeItem);
+    // FILTER EVENT
+    filter.addEventListener('keyup', filterItems);
 
-// FILTER EVENT
-filter.addEventListener('keyup',filterItems);
+    // ADD ITEM
+    function addItem(e) {
+      e.preventDefault();
 
-// ADD ITEM
+      // GET INPUT VALUES
+      var newItem = document.getElementById('item').value;
+      var newDescription = document.getElementById('description').value;
 
-function addItem(e){
-    e.preventDefault();
-    
-    // GET INPUT VALUE
-    var newItem=document.getElementById('item').value;
+      // CREATE NEW LI ELEMENT
+      var li = document.createElement('li');
 
-    // CREATE NEW LI ELEMENT
-    var li=document.createElement('li');
+      // ADD CLASS
+      li.className = 'list-group-item';
 
-    // ADD CLASS
-    li.className = 'list-group-item';
-    console.log(li);
+      // ADD TEXT NODE WITH INPUT VALUES
+      li.innerHTML = `<strong>${newItem}</strong> ${newDescription}`;
 
-    // ADD TEXT NODE WITH INPUT VALUE
-    li.appendChild(document.createTextNode(newItem));
+      // CREATE DEL BUTTON ELEMENT
+      var deleteBtn = document.createElement('button');
 
-    // CREATE DEL BUTTON ELEMENT
-    var deleteBtn = document.createElement('button');
+      // ADD CLASSES TO DELETE BUTTON
+      deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-    // ADD CLASSES TO DELETE BUTTON
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+      // APPEND TEXT NODE
+      deleteBtn.appendChild(document.createTextNode('x'));
 
-    // APPEND TEXT NODE
-    deleteBtn.appendChild(document.createTextNode('x'));
+      // APPEND BUTTON TO LI
+      li.appendChild(deleteBtn);
 
-    //APPEND BUTTON TO LI
-    li.appendChild(deleteBtn);
-
-    // APPEND LI TO LIST
-    itemList.appendChild(li);
-
-
-}
-
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are you Sure?')){
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
+      // APPEND LI TO LIST
+      itemList.appendChild(li);
     }
-}
 
-
-// FILTER ITEMS
-
-function filterItems(e) {
-    // CONVERT TEXT TO LOWERCASE
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName('li');
-    // CONVERT TO AN ARRAY
-    Array.from(items).forEach(function(item) { // Corrected the closing parenthesis here
-        var itemName = item.firstChild.textContent;
-
-        if (itemName.toLowerCase().indexOf(text) !== -1) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
+    function removeItem(e) {
+      if (e.target.classList.contains('delete')) {
+        if (confirm('Are you Sure?')) {
+          var li = e.target.parentElement;
+          itemList.removeChild(li);
         }
-    });
-}
+      }
+    }
+
+    // FILTER ITEMS
+    function filterItems(e) {
+      // CONVERT TEXT TO LOWERCASE
+      var text = e.target.value.toLowerCase();
+      var items = itemList.getElementsByTagName('li');
+
+      Array.from(items).forEach(function (item) {
+        var itemText = item.textContent.toLowerCase();
+        if (itemText.includes(text)) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none';
+        }
+      });
+    }
